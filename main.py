88e88f113f982e9d5ac67c9888fa8c66a12b0894
Meg <41833919+SpicyCayenne@users.noreply.github.com>
2020-03-28 20:08:14 -0700

    Updated tower placement, screen scaling
    game scales to screen size correctly. Added ability to select between two different types of towers.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def run_game():
 
     display_info = pygame.display.Info()
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, 32)
+    game_surface = screen.copy()
 
     towers = []
 
@@ -22,16 +23,34 @@ def run_game():
 
     def update_display():
         """Update the game display"""
+        scaling_info = get_scaling_info()[0]
         screen.fill(GREEN)
         for tower in towers:
             tower.draw()
+        screen.blit(pygame.transform.scale(
+            game_surface, (scaling_info.current_w, scaling_info.current_h)), (0, 0))
         pygame.display.update()
 
-    def create_tower():
-        towers.append(Tower(screen, "./assets/tower.png", 400, 400))
+    def create_tower(tower_type):
+        x_scale, y_scale = get_scaling_info()[1:]
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        tower_image = ["./assets/", tower_type, "_tower.png"]
+        tower_image_path = "".join(tower_image)
+        towers.append(Tower(
+            game_surface, tower_image_path, mouse_x * x_scale, mouse_y * y_scale))
 
+    def select_tower(keypress):
+        selection = {49:"elf", 50:"dwarf"}
+        return selection[keypress]
+
+    def get_scaling_info():
+        scaling_info = pygame.display.Info()
+        x_ratio = display_info.current_w/scaling_info.current_w
+        y_ratio = display_info.current_h/scaling_info.current_h
+        return scaling_info, x_ratio, y_ratio
 
     run = True
+    tower_selection = 0
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -41,9 +60,12 @@ def run_game():
                     run = False
                 if event.key == pygame.K_F11:
                     toggle_fullscreen()
-                if event.key == pygame.K_SPACE:
-                    create_tower()
-                    print(towers)
+                if event.key == pygame.K_1 or event.key == pygame.K_2:
+                    tower_selection = select_tower(event.key)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                valid_towers = ["elf", "dwarf"]
+                if tower_selection in valid_towers: #to stop the game crashing if the user hasn't selected a tower yet
+                    create_tower(tower_selection)
 
         update_display()
 
--- tower.py
+++ tower.py
@@ -5,13 +5,14 @@ class Tower:
     """Tower information"""
     def __init__(self, display_surface, image, x, y):
         self.display_surface = display_surface
-        self.image = pygame.image.load(image)
-        self.x = x
-        self.y = y
+        self.image = pygame.image.load(image).convert()
+        self.x_coord = x
+        self.y_coord = y
 
     def draw(self):
         """Draws the tower on teh screen using the specified image at coordinates x and y"""
-        self.display_surface.blit(self.image, (self.x, self.y))
+        self.image = pygame.transform.scale(self.image, (10, 10))
+        self.display_surface.blit(self.image, (self.x_coord, self.y_coord))
 
     def attack(self):
-        pass
+        """Causes the tower to attack enemies in range"""

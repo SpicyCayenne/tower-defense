a19a3eb34547208e8599345efb30628c4cea3c90
Meg <41833919+SpicyCayenne@users.noreply.github.com>
2020-03-30 21:50:22 -0700

    Added Player class

---
+++ player.py
@@ -0,0 +1,14 @@
+"""Class for storing all the player's info"""
+class Player:
+    """Class for storing all the player's info"""
+    def __init__(self, score, lives):
+        self.score = score
+        self.lives = lives
+
+    def update_score(self, amount):
+        """Updates the current score by the given amount"""
+        self.score += amount
+
+    def lose_life(self):
+        """Decrements the player's life total"""
+        self.lives -= 1
